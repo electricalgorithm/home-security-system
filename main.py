b41ccd0f0ce10305198989f901dfae863eeee140
Gökhan Koçmarlı <gokhan.kocmarli@gmail.com>
2024-04-21 23:20:55 +0300

    refactor: configure for telegram send

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Main application layer for Home Security System.
 import json
 import sys
 from concurrent.futures import wait
+from time import sleep
 from typing import Any
 
 from core.observers.observer.hss_observer import HomeSecuritySystemObserver
@@ -11,9 +12,11 @@ from core.observers.subject.eye_subject import EyeSubject
 from core.observers.subject.wifi_subject import WiFiSubject
 from core.strategies.detectors.efficientdet_strategy import EfficientdetStrategy
 from core.strategies.eye.picamera_strategy import PiCameraStrategy
+from core.strategies.notifier.telegram_strategy import TelegramStrategy
 from core.strategies.notifier.whatsapp_strategy import WhatsappStrategy
 from core.strategies.wifi.admin_panel_strategy import AdminPanelStrategy
-from core.utils.datatypes import Protector, WhatsappReciever
+from core.utils.datatypes import Protector, TelegramReciever
+from core.utils.fileio_adaptor import upload_to_fileio
 
 
 def read_configurations() -> dict[str, Any]:
@@ -35,21 +38,20 @@ def main():
     # Read configurations.
     config, strategy_config = read_configurations()
     # Create a WhatsApp notifier.
-    whatsapp_notifier = WhatsappStrategy()
+    notifier = TelegramStrategy(strategy_config['telegram_strategy']['bot_key'])
     for reciever in config['recievers']:
-        whatsapp_notifier.add_reciever(WhatsappReciever(reciever['name'],
-                                                        reciever['tel_no'],
-                                                        reciever['callmebot_key']))
+        notifier.add_reciever(TelegramReciever(reciever['name'],
+                                               reciever['chat_id']))
 
     # Create a Protector within IpAddressStrategy.
-    ip_address_strategy = AdminPanelStrategy(strategy_config)
+    network_strategy = AdminPanelStrategy(strategy_config['admin_panel_strategy'])
     for protector in config['protectors']:
-        ip_address_strategy.add_protector(Protector(protector['name'],
-                                                    protector['address']))
+        network_strategy.add_protector(Protector(protector['name'],
+                                                 protector['address']))
 
     # Create observer.
     hss_observer = HomeSecuritySystemObserver()
-    hss_observer.set_notifier(whatsapp_notifier)
+    hss_observer.set_notifier(notifier)
 
     #  Create subjects to observe.
     wifi_subject = WiFiSubject()
@@ -58,7 +60,7 @@ def main():
     eye_subject.attach(hss_observer)
 
     # Run subjects.
-    wifi_subject.run(ip_address_strategy)
+    wifi_subject.run(network_strategy)
 
     # Set-up the camera to detect humans.
     camera = PiCameraStrategy()
@@ -66,15 +68,24 @@ def main():
     eye_subject.run(camera, wifi_subject.get_protector_lock())
 
     # Notify that the system is running.
-    whatsapp_notifier.notify_all("Home Security System is started.")
+    notifier.notify_all("Home Security System is started.")
+    sleep(5)
+    
+    if isinstance(notifier, TelegramStrategy):
+        with open(f"{eye_subject._image_path}/initial_frame.jpg", "rb") as initial_frame:
+            notifier.send_image_all(initial_frame)
+    elif isinstance(notifier, WhatsappStrategy):
+        fileio_link = upload_to_fileio(initial_frame)
+        notifier.notify_all(f"Here is the initial frame: {fileio_link}.")
 
     # Wait for the futures.
-    _, failures = wait([wifi_subject.thread, eye_subject.thread])
+    _, failures = wait([wifi_subject.thread, eye_subject.thread], return_when="FIRST_COMPLETED")
     for failure in failures:
-        whatsapp_notifier.notify_all("Home Security System has failed to run. "
+        notifier.notify_all("Home Security System has failed to run. "
                                      "Please check the logs.")
-        whatsapp_notifier.notify_all("Error: " + str(failure.exception()))
-        whatsapp_notifier.notify_all("Result: " + str(failure.result()))
+        notifier.notify_all("Failure: " + str(failure))
+        notifier.notify_all("Error: " + str(failure.exception()))
+        notifier.notify_all("Result: " + str(failure.result()))
         # Close the application to let systemd re-start it.
         sys.exit(1)
 
