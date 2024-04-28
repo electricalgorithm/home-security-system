d910da45c9ac09b4a929d62190ce2f914602b846
Gyokhan Kochmarla <gokhan.kocmarli@gmail.com>
2024-04-29 01:07:23 +0300

    fix: Too many open files (#16)
    * fix: create session with pool connections
    
    * feat: add retries amount and backoff method

--- core/strategies/wifi/admin_panel_strategy.py
+++ core/strategies/wifi/admin_panel_strategy.py
@@ -4,6 +4,8 @@ The strategy which searches for MAC addresses using Admin Panel.
 from typing import Any
 
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 from bs4 import BeautifulSoup
 
 from core.strategies.wifi.base_wifi_strategy import BaseWiFiStrategy
@@ -23,6 +25,15 @@ class AdminPanelStrategy(BaseWiFiStrategy):
         """Constructor for AdminPanelStrategy."""
         super().__init__()
         self._login_data: dict[str, Any] = login_data
+        self._session: requests.Session = requests.Session()
+
+        # Create a HTTP adapter to limit the number of connections.
+        retries = Retry(total=3, backoff_factor=0.3, backoff_max=5.0)
+        http_adaptor: HTTPAdapter = HTTPAdapter(
+            pool_connections=1, max_retries=retries
+        )
+        self._session.mount("http://", http_adaptor)
+        self._session.mount("https://", http_adaptor)
 
     def check_protectors(self) -> WiFiStrategyResult:
         """This method checks if there are any protectors around."""
@@ -38,9 +49,8 @@ class AdminPanelStrategy(BaseWiFiStrategy):
     def _get_all_connected(self) -> list[ConnectedDeviceResult]:
         """This method returns a list of addresses of the clients connected to the network."""
         # Create a session to store cookies.
-        session = requests.Session()
-        session.get("http://192.168.1.95/login_security.html")
-        session.post(
+        self._session.get("http://192.168.1.95/login_security.html")
+        self._session.post(
             "http://192.168.1.95/Forms/login_security_1",
             headers={
                 "Content-Type": "application/x-www-form-urlencoded",
@@ -50,7 +60,8 @@ class AdminPanelStrategy(BaseWiFiStrategy):
         )
 
         # Get the response for the page with MAC address list.
-        response = session.get("http://192.168.1.95/status/status_deviceinfo.htm")
+        response = self._session.get(
+            "http://192.168.1.95/status/status_deviceinfo.htm")
 
         # Parse the response.
         soup = BeautifulSoup(response.text, "html.parser")
@@ -62,7 +73,6 @@ class AdminPanelStrategy(BaseWiFiStrategy):
             for element in tabdata
             if len(element.text) == 17
         ]
-        session.close()
 
         logger.debug("Connected devices: %s", str(mac_addrs))
         return [ConnectedDeviceResult(mac_addr.upper()) for mac_addr in mac_addrs]
